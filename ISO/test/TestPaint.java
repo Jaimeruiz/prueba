227b7ae98e1d2f19599419e6d86509489d6017a2
Jaime Ruiz-Ogarrio Matías <j.ruizogarrio@usp.ceu.es>
2023-01-17 18:38:41 +0100

    Primer Test resuelto, clase lineas creada

--- ISO/test/TestPaint.java
+++ ISO/test/TestPaint.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import principal.Lineas;
 
 /**
  *
@@ -18,6 +19,11 @@ import static org.junit.Assert.*;
 public class TestPaint {
     
     public TestPaint() {
+    }
+    
+    @Test
+    public void Lineas() {
+        Lineas lineas = new Lineas();
     }
     
     @BeforeClass
