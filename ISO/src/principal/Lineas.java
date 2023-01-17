f29b1f829ea073d2e54edc1ae8a4d74dfe587d22
Jaime Ruiz-Ogarrio Matías <j.ruizogarrio@usp.ceu.es>
2023-01-17 18:46:14 +0100

    Tercer commit, constructor y metodo para dibujar añadidos

--- ISO/src/principal/Lineas.java
+++ ISO/src/principal/Lineas.java
@@ -5,6 +5,8 @@
  */
 package principal;
 
+import java.awt.Graphics;
+
 /**
  *
  * @author jaime
@@ -12,8 +14,17 @@ package principal;
 public class Lineas {
 
   private int x1, y1, x2, y2;
+
+    public Lineas(int x1, int y1, int x2, int y2) {
+        setX1(x1);
+        setY1(y1);
+        setX2(x2);
+        setY2(y2);
+    }
   
-  
+   public void pintar(Graphics grafico){
+       grafico.drawLine(getX1(), getY1(), getX2(), getY2());
+   }
 
     public int getX1() {
         return x1;
